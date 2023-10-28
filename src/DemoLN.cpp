d84701b6adfbe0219b47f956320c7fa250f21056
Anvie <sinji.bash@gmail.com>
2023-10-29 00:23:01 +0500

    Жоска навалил Вершинного Шейдера

--- src/DemoLN.cpp
+++ src/DemoLN.cpp
@@ -5,20 +5,26 @@
 #include <glm/glm.hpp>
 #include <fstream>
 #include <sstream>
+#include <vector>
 #include <string>
+#include <tuple>
 
-GLuint getShader(const char* FilePath)
+struct shaderConstructData 
+{
+	const char* filePath;
+	GLuint type;
+};
+
+GLuint CreateShader(const char* FilePath, GLuint type) 
 {
 	// ---------- Загрузка кода шейдера
 	std::string sourceString;
 	std::ifstream sourceFile;
-	//char* code = static_cast<char*>(malloc(sizeof(char)*2000));
 
 	char* code;
 
 	sourceFile.exceptions(std::ifstream::badbit);
 	try {
-
 		sourceFile.open(FilePath);
 
 		if (!sourceFile.is_open())
@@ -31,41 +37,58 @@ GLuint getShader(const char* FilePath)
 		sourceFile.close();
 		sourceString = sourceStream.str();
 	}
-	catch(std::ifstream::failure e) 
-	{ 
-		std::cout << "ERROR::SHADER::FILE_NOT_SUCCESFULLY_READ" << std::endl; 
+	catch (std::ifstream::failure e)
+	{
+		std::cout << "ERROR::SHADER::FILE_NOT_SUCCESFULLY_READ" << std::endl;
 	}
 	code = const_cast<char*>(sourceString.c_str());
+
+	//std::cout << code << std::endl;
 	
 	// ---------- Компиляция шейдера
-	GLuint shader = glCreateShader(GL_FRAGMENT_SHADER);
-
-	glShaderSource(shader,1,&code,NULL);
-	glCompileShader(shader);
-
-	/*
-	glGetShaderiv(shader,GL_COMPILE_STATUS, &ok);
-	if (!ok)
-	{
-		glGetShaderInfoLog(shader,2000,NULL,log);
-		std::cout << log << std::endl;
-	}
-	*/
-
-	// ---------- Сборка программы
 	GLint ok;
 	GLchar log[2000];
 
-	GLuint program = glCreateProgram();
-	glAttachShader(program,shader);
-	glLinkProgram(program);
+	GLuint shader = glCreateShader(type);
+	glShaderSource(shader, 1, &code, NULL);
+	glCompileShader(shader);
 
-	glGetProgramiv(program, GL_COMPILE_STATUS, &ok);
-	if (!ok)
-	{
-		glGetProgramInfoLog(program, 2000, NULL, log);
-		std::cout << log << std::endl;
+	glGetShaderiv(shader, GL_COMPILE_STATUS, &ok);
+
+	// Проверка на правильность компиляции
+	if (!ok) {
+		glGetShaderInfoLog(shader, 2000, NULL, log);
+		std::cout << "Ошибка комплиции шейдера:" << std::endl << log << std::endl;
 	}
+
+	return shader;
+}
+
+GLuint getShaderProgram(std::vector<shaderConstructData> PathesAndTypes)
+{
+	std::vector<GLuint> shaders;
+
+	for (int i = 0; i < PathesAndTypes.size(); ++i) {
+		shaders.push_back(CreateShader(PathesAndTypes[i].filePath, PathesAndTypes[i].type));
+	}
+
+	GLint ok;
+	GLchar log[2000];
+	// ---------- Сборка программы
+	GLuint program = glCreateProgram();
+	for (int i = 0; i < shaders.size(); ++i) {
+		glAttachShader(program, shaders[i]);
+	}
+	
+	glLinkProgram(program);
+	glGetProgramiv(program, GL_COMPILE_STATUS, &ok);
+
+	// Проверка на правильность компиляции
+	if (!ok) {
+		glGetProgramInfoLog(program, 2000, NULL, log);
+		std::cout << "Ошибка комплиции шейдерной программы:" << std::endl << log << std::endl;
+	}
+
 	return program;
 }
 
@@ -82,23 +105,25 @@ void draw(GLuint vertexVBO, GLuint colorVBO)
 
 	glEnableClientState(GL_VERTEX_ARRAY);
 	glEnableClientState(GL_COLOR_ARRAY);
-		glDrawArrays(GL_TRIANGLE_FAN, 0, 3);
+		glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
 	glDisableClientState(GL_VERTEX_ARRAY);
 	glDisableClientState(GL_COLOR_ARRAY);
 }
 
 int main()
 {
+	setlocale(LC_ALL, "Russian");
 	if (!CORE::Window::init(500, 500, "Game"))
 		return 0;
 
 	float vertices[] = {
 	-0.5f, -0.5f, 0.0f,
 	 0.5f, -0.5f, 0.0f,
-	 0.0f,  0.5f, 0.0f
+	 0.5f,  0.5f, 0.0f,
+	-0.5f,  0.5f, 0.0f
 	};
 
-	float color[] = { 0.7f,0,0.9f, 0,1,0, 0,0,1};
+	float color[] = { 0.7f,0,0.9f, 0,1,0, 0,0,1, 1,0,0};
 	
 	GLuint vertexVBO;
 	GLuint colorVBO;
@@ -113,8 +138,9 @@ int main()
 		glBufferData(GL_ARRAY_BUFFER, sizeof(color), color, GL_STATIC_DRAW);
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	
-	GLuint prog = getShader("res/shaders/test.frag");
+	GLuint prog = getShaderProgram({ {"res/shaders/VertexShader.glsl", GL_VERTEX_SHADER} , {"res/shaders/ColorShader.glsl", GL_FRAGMENT_SHADER} });
 	glUseProgram(prog);
+	
 
 	while (!CORE::Window::isShouldClose())
 	{
