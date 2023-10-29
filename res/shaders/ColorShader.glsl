2a12f46bd544105a588f6852bf770e7ccc7925f3
Anvie <sinji.bash@gmail.com>
2023-10-29 23:04:02 +0500

    cringe

--- res/shaders/ColorShader.glsl
+++ res/shaders/ColorShader.glsl
@@ -3,5 +3,4 @@
 void main() 
 {
     gl_FragColor = vec4(0.3,0,0.8,1);
-
 }
--- src/DemoLN.cpp
+++ src/DemoLN.cpp
@@ -1,4 +1,7 @@
-﻿#include <iostream>
+﻿#ifndef SHADER_H
+#define SHADER_H
+
+#include <iostream>
 #include "Window/window.hpp"
 #include <stdio.h>
 #include <iostream>
@@ -8,6 +11,7 @@
 #include <vector>
 #include <string>
 #include <tuple>
+#include <glm/ext/matrix_transform.hpp>
 
 struct shaderConstructData 
 {
@@ -15,36 +19,40 @@ struct shaderConstructData
 	GLuint type;
 };
 
-GLuint CreateShader(const char* FilePath, GLuint type) 
+class Shader 
 {
-	// ---------- Загрузка кода шейдера
-	std::string sourceString;
-	std::ifstream sourceFile;
-
-	char* code;
-
-	sourceFile.exceptions(std::ifstream::badbit);
-	try {
-		sourceFile.open(FilePath);
-
-		if (!sourceFile.is_open())
-		{
-			std::cout << "Shader is not found" << std::endl;
-		}
-
-		std::stringstream sourceStream;
-		sourceStream << sourceFile.rdbuf();
-		sourceFile.close();
-		sourceString = sourceStream.str();
-	}
-	catch (std::ifstream::failure e)
+private:
+std::string readFile(const char* FilePath)
 	{
-		std::cout << "ERROR::SHADER::FILE_NOT_SUCCESFULLY_READ" << std::endl;
-	}
-	code = const_cast<char*>(sourceString.c_str());
+		// ---------- Загрузка кода шейдера
+		std::string sourceString;
+		std::ifstream sourceFile;
 
-	//std::cout << code << std::endl;
-	
+		sourceFile.exceptions(std::ifstream::badbit);
+		try {
+			sourceFile.open(FilePath);
+
+			if (!sourceFile.is_open())
+			{
+				std::cout << "Shader is not found" << std::endl;
+			}
+
+			std::stringstream sourceStream;
+			sourceStream << sourceFile.rdbuf();
+			sourceFile.close();
+			sourceString = sourceStream.str();
+		}
+		catch (std::ifstream::failure e)
+		{
+			std::cout << "ERROR::SHADER::FILE_NOT_SUCCESFULLY_READ" << std::endl;
+		}
+		return sourceString;
+	}
+GLuint CreateShader(const char* FilePath, GLuint type)
+{
+	std::string stringCode = readFile(FilePath);
+	char* code = const_cast<char*>(stringCode.c_str());
+
 	// ---------- Компиляция шейдера
 	GLint ok;
 	GLchar log[2000];
@@ -58,40 +66,46 @@ GLuint CreateShader(const char* FilePath, GLuint type)
 	// Проверка на правильность компиляции
 	if (!ok) {
 		glGetShaderInfoLog(shader, 2000, NULL, log);
-		std::cout << "Ошибка комплиции шейдера:" << std::endl << log << std::endl;
+		std::cout << "Ошибка комплиции шейдера:" << std::endl
+			<< log << std::endl
+			<< "Код шейдера:" << std::endl
+			<< code << std::endl;
 	}
 
 	return shader;
 }
+public:
+	GLuint program;
+	Shader(std::vector<shaderConstructData> PathesAndTypes)
+	{
+		std::vector<GLuint> shaders;
 
-GLuint getShaderProgram(std::vector<shaderConstructData> PathesAndTypes)
-{
-	std::vector<GLuint> shaders;
+		for (int i = 0; i < PathesAndTypes.size(); ++i) {
+			shaders.push_back(CreateShader(PathesAndTypes[i].filePath, PathesAndTypes[i].type));
+		}
 
-	for (int i = 0; i < PathesAndTypes.size(); ++i) {
-		shaders.push_back(CreateShader(PathesAndTypes[i].filePath, PathesAndTypes[i].type));
+		GLint ok;
+		GLchar log[2000];
+		// ---------- Сборка программы
+		program = glCreateProgram();
+		for (int i = 0; i < shaders.size(); ++i) {
+			glAttachShader(program, shaders[i]);
+		}
+
+		glLinkProgram(program);
+		glGetProgramiv(program, GL_COMPILE_STATUS, &ok);
+
+		// Проверка на правильность компиляции
+		if (!ok) {
+			glGetProgramInfoLog(program, 2000, NULL, log);
+			std::cout << "Ошибка комплиции шейдерной программы:" << std::endl << log << std::endl;
+		}
 	}
-
-	GLint ok;
-	GLchar log[2000];
-	// ---------- Сборка программы
-	GLuint program = glCreateProgram();
-	for (int i = 0; i < shaders.size(); ++i) {
-		glAttachShader(program, shaders[i]);
+	void use()
+	{
+		glUseProgram(program);
 	}
-	
-	glLinkProgram(program);
-	glGetProgramiv(program, GL_COMPILE_STATUS, &ok);
-
-	// Проверка на правильность компиляции
-	if (!ok) {
-		glGetProgramInfoLog(program, 2000, NULL, log);
-		std::cout << "Ошибка комплиции шейдерной программы:" << std::endl << log << std::endl;
-	}
-
-	return program;
-}
-
+};
 
 void draw(GLuint vertexVBO, GLuint colorVBO)
 {
@@ -100,7 +114,7 @@ void draw(GLuint vertexVBO, GLuint colorVBO)
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 
 	glBindBuffer(GL_ARRAY_BUFFER, colorVBO);
-		glColorPointer(3, GL_FLOAT, 0, NULL);
+	  	glColorPointer(3, GL_FLOAT, 0, NULL);
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 
 	glEnableClientState(GL_VERTEX_ARRAY);
@@ -116,43 +130,43 @@ int main()
 	if (!CORE::Window::init(500, 500, "Game"))
 		return 0;
 
-	float vertices[] = {
+	std::vector<GLfloat> vertices = {
 	-0.5f, -0.5f, 0.0f,
 	 0.5f, -0.5f, 0.0f,
 	 0.5f,  0.5f, 0.0f,
 	-0.5f,  0.5f, 0.0f
 	};
 
-	float color[] = { 0.7f,0,0.9f, 0,1,0, 0,0,1, 1,0,0};
+	std::vector<GLfloat> color = { 0.7f,0,0.9f, 0,1,0, 0,0,1, 0.3f,0,0.5f};
 	
-	GLuint vertexVBO;
-	GLuint colorVBO;
+	GLuint vetexVBO, colorVBO;
 
-	glGenBuffers(1, &vertexVBO);
-	glBindBuffer(GL_ARRAY_BUFFER, vertexVBO);
-		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glGenBuffers(1, &vetexVBO);
+	glBindBuffer(GL_ARRAY_BUFFER, vetexVBO);
+		glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(GLfloat), vertices.data(), GL_STATIC_DRAW);
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 
 	glGenBuffers(1, &colorVBO);
 	glBindBuffer(GL_ARRAY_BUFFER, colorVBO);
-		glBufferData(GL_ARRAY_BUFFER, sizeof(color), color, GL_STATIC_DRAW);
+		glBufferData(GL_ARRAY_BUFFER, color.size() * sizeof(GLfloat), color.data(), GL_STATIC_DRAW);
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	
-	GLuint prog = getShaderProgram({ {"res/shaders/VertexShader.glsl", GL_VERTEX_SHADER} , {"res/shaders/ColorShader.glsl", GL_FRAGMENT_SHADER} });
-	glUseProgram(prog);
-	
+	Shader ourShader ({ {"res/shaders/VertexShader.glsl", GL_VERTEX_SHADER}, {"res/shaders/ColorShader.glsl", GL_FRAGMENT_SHADER} });
+	ourShader.use();
 
 	while (!CORE::Window::isShouldClose())
 	{
 		CORE::Window::pollEvents();
 		glPushMatrix();
-			draw(vertexVBO,colorVBO);
+			draw(vetexVBO,colorVBO);
 		glPopMatrix();
 		CORE::Window::swapBuffers();
 	}
 
-	glDeleteBuffers(1,&vertexVBO);
+	glDeleteBuffers(1, &vetexVBO);
 	glDeleteBuffers(1, &colorVBO);
 	CORE::Window::final();
 	return 0;
 }
+
+#endif
